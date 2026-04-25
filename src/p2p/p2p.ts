651e79f488989b4310436686294613a853d7aaf4
Ander Íñiguez Bustillo <anderibus@gmail.com>
2026-04-25 20:05:49 +0200

    ayuda

--- src/p2p/p2p.ts
+++ src/p2p/p2p.ts
@@ -1,9 +1,59 @@
 // @ts-ignore
-import Autopass from 'autopass'
+import Autopass from 'autopass';
 
 // @ts-ignore
 import Corestore from 'corestore';
 
-const store = new Corestore('./corestore.d');
+export class P2P {
+    private atpass: Autopass;
+    private crstore: Corestore;
 
-await store.ready();
+    constructor(storagePath: string) {
+        this.crstore = new Corestore(storagePath);
+    }
+
+    public async initializeAsCreator(): Promise<string> {
+        this.atpass = new Autopass(this.crstore);
+        await this.atpass.ready();
+        return await this.atpass.createInvite();
+    }
+
+    public async initializeAsInvitee(inviteCode: string): Promise<void> {
+        const pair = Autopass.pair(this.crstore, inviteCode);
+        this.atpass = await pair.finished();
+        await this.atpass.ready();
+    }
+
+    public async saveItem(prefix: string, id: string | number, 
+                          item: any): Promise<void> {
+        const key = `${prefix}:${id}`; 
+        const value = JSON.stringify(item);
+        
+        await this.atpass.add(key, value);
+    }
+
+    public listenForUpdates(onDataReceived: (key: string, data: any) => 
+                            void): void {
+        this.atpass.on('update', async () => {
+            for await (const { key, value } of this.atpass.list()) {
+                const parsedData = JSON.parse(value.toString());
+                onDataReceived(key, parsedData);
+            }
+        });
+    }
+
+    public async getAllItems(): Promise<Array<{key: string, data: any}>> {
+        const items = [];
+        for await (const { key, value } of this.atpass.list()) {
+            const parsedData = JSON.parse(value.toString());
+            items.push({ key, data: parsedData });
+        }
+        return items;
+    }
+
+    public async deleteItem(prefix: string, id: string | number): Promise<void> {
+        const key = `${prefix}:${id}`;
+        const tombstone = JSON.stringify({ _deleted: true });
+        await this.atpass.add(key, tombstone);
+    }
+}
